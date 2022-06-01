afa7e2f725a792e05b439a1634700b6440951f7e
King <contact.carlosel@gmail.com>
2022-06-01 17:08:20 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropdown from './components/dropdown/Dropdown';
+
+const dropdownItems = [
+  {title : "Index 1", value : 1 },
+  {title : "Index 2", value : 2 },
+  {title : "Index 3", value : 3 }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{marginTop : '50px', marginLeft : '50px', width : '200px'}}>
+        <Dropdown items={dropdownItems} default={1}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/dropdown/Dropdown.tsx
@@ -0,0 +1,65 @@
+import './style.scss'
+import React, { useState, useEffect,useRef } from 'react';
+import { isTemplateSpan } from 'typescript';
+import NarrowIcon from '../icons/NarrowIcon'
+
+interface DropdownProps {
+    items : {title : string, value : any }[]
+    default?: number
+}
+
+const Dropdown = (props : DropdownProps) => {
+    const [textValue, setTextValue] = useState("Select the Index");
+    const [showItems, setShowItems] = useState(0);
+    const dropBox = useRef<any>();
+
+    useEffect(() => {
+        if(props.items.length > 0){
+            if(props.default){
+                setTextValue(props.items[props.default - 1].title)
+            } else {
+                setTextValue(props.items[0].title)
+            }
+        }
+    }, [])
+
+    const handle = (val : string) => {
+        setTextValue(val);
+        setShowItems(0);
+    }
+    useEffect(() => {
+        const handleClickEvent = (event: any) => {
+            if (dropBox?.current && !dropBox.current.contains(event.target)) {
+                setShowItems(0);
+            }
+        };
+        window.addEventListener("click", handleClickEvent);
+        return () => {
+            window.removeEventListener("click", handleClickEvent);
+        };
+    }, []);
+
+    return (
+        <div className='dropdown-container' ref={dropBox}>
+            <div className='dropdown-header' onClick={() => setShowItems(1)}>
+                <div>
+                    {textValue}
+                </div>
+                <div>
+                    <NarrowIcon />
+                </div>
+            </div>
+            <div className='dropdown-items' style={{opacity : showItems}}>
+                {
+                    props.items.map((row : any, index) => {
+                        return (
+                            <div key={index} className='dropdown-item' onClick={() => handle(row.title)}>{row.title}</div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown;
